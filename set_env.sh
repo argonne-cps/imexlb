31bbfcb4ba6f962b9f345a7cbc8dcbbffa93808d
Saumil Sudhir Patel <sspatel@thetagpu19.mcp.alcf.anl.gov>
2023-08-29 11:10:33 -0500

    update

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+
+#IF using ThetaGPU
 module load openmpi/openmpi-4.1.0_ucx-1.10.0
-export OMPI_MPICXX=/grand/IMEXLBM/czhao/Kokkos/kokkos/bin/nvcc_wrapper
+#or
+module load openmpi/openmpi-4.1.0_ucx-1.11.0_gcc-9.3.0
+#export OMPI_MPICXX=/grand/IMEXLBM/czhao/Kokkos/kokkos/bin/nvcc_wrapper
